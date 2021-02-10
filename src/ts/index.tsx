48d624e323aed45018ddae6946415556e0e58d9d
Jake Howard <git@theorangeone.net>
2021-02-10 17:07:22 +0000

    Add basic markup

--- src/ts/index.tsx
+++ src/ts/index.tsx
@@ -1,8 +1,50 @@
 import { h, render } from "preact";
+import { PureComponent } from "preact/compat";
+import { AwesomeButton } from "react-awesome-button";
 
-import App from "./app";
+interface State {
+  value: string | null;
+}
+
+export default class App extends PureComponent<{}, State> {
+  constructor() {
+    super();
+    this.state = { value: null };
+  }
+
+  onGenerate = () => {
+    this.setState({
+      value: "42"
+    });
+  };
+
+  render() {
+    return (
+      <div className="app-wrapper">
+        <input placeholder="Click generate" disabled value={this.state.value} />
+        <div>
+          <AwesomeButton
+            ripple
+            type="primary"
+            size="large"
+            onPress={this.onGenerate}
+          >
+            Generate key
+          </AwesomeButton>
+          <AwesomeButton
+            ripple
+            type="secondary"
+            size="large"
+            disabled={!this.state.value}
+          >
+            Copy to clipboard
+          </AwesomeButton>
+        </div>
+      </div>
+    );
+  }
+}
 
 document.addEventListener("DOMContentLoaded", () => {
-  const target = document.getElementById("app");
-  render(<App />, target);
+  render(<App />, document.getElementById("app"));
 });
