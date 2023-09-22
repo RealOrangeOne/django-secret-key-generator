9a79ebc895ee66cef86841aa800ad571ddee9055
Jake Howard <git@theorangeone.net>
2023-09-22 13:18:42 +0100

    Improve random value generation

--- src/ts/random.ts
+++ src/ts/random.ts
@@ -4,13 +4,16 @@ const CHARS = "abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)".split("");
 const KEY_LENGTH = 50;
 
 export function generateRandomValue(min: number, max: number): number {
-  const sampleSize = max - min + 1;
-  const upper = RAND_MAX - (RAND_MAX % sampleSize);
+  const generatorMax = max - min;
+  if (generatorMax > RAND_MAX) {
+    throw "Generating range is too large";
+  }
+
   let randomValue: number;
   do {
     randomValue = window.crypto.getRandomValues(new Uint8Array(1))[0];
-  } while (randomValue >= upper);
-  randomValue %= sampleSize;
+  } while (randomValue > generatorMax);
+
   return min + randomValue;
 }
 
